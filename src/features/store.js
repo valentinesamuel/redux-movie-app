387f885b181f1fd1170f4635e39d0e7f6a0068d7
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-03 21:17:29 +0100

    ✔FEAT: Successfully persisting state without console errors

--- src/features/store.js
+++ src/features/store.js
@@ -1,7 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore,combineReducers } from "@reduxjs/toolkit";
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE,persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from './user/userSlice'
+
+
+const rootReducer = combineReducers({
+  userSlice: userReducer
+
+})
+
+const persistConfig = {
+  key: 'root',
+
+  storage
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-    reducer: {
-      userSlice: userReducer
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+         PERSIST,
+          "userData/loginWithGooglePopup/fulfilled",
+        ],
+        ignoredPaths: ["userSlice.userData"],
+      },
+    }),
 });
+
+export const persistor = persistStore(store)

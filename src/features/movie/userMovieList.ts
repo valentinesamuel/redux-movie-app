5e18aa3679c4eb92abdef16d62bdb09103b5b625
valentinesamuel <valentinesamuel2580@gmail.com>
2022-08-24 22:49:07 +0100

    ⚒  removed unused import

--- src/features/movie/userMovieList.ts
+++ src/features/movie/userMovieList.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice, PayloadAction, } from "@reduxjs/toolkit";
 import { getUserMovieList } from "../../utilities/firebase"
-import { Movie, UserMovieList, UserMovieListInitialState } from "./movie.types";
+import { Movie, UserMovieListInitialState } from "./movie.types";
 
 const initialState:UserMovieListInitialState = {
     listOfMovies: [],
